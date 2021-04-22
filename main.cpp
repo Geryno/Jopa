2bbba5399e7b1f59a4c6cac0eaa5147e83bcb5cc
Алексей Подгайный <gerynochannel@gmail.com>
2021-04-22 23:16:53 +0300

    love you lina

--- main.cpp
+++ main.cpp
@@ -1,24 +1,31 @@
+#define SCALE_STEP 14
+
 #include <iostream>
 #include <vector>
+#include <fstream>          // Вывод в .csv
 
 using namespace std;
 
-const int base_n = 7;
-const int newV = base_n * 2;
+const int base_n = 3;
+const int newV = base_n * SCALE_STEP;
 
 std::vector<std::vector<int>> create2DArray(unsigned height, unsigned width)
 {
-   return std::vector<std::vector<int>>(height, std::vector<int>(width, 0));
+    return std::vector<std::vector<int>>(height, std::vector<int>(width, 0));
 }
 
-std::vector<std::vector<int>> create_base_m(unsigned height, unsigned width)
+std::vector<std::vector<int>> create_base_m()
 {
-   return std::vector<std::vector<int>>(height, std::vector<int>(width, 1));
+    std::vector<std::vector<int>> base_m;
+    return base_m = {
+                { 1, 1, 1 },
+                { 1, 1, 1 },
+                { 1, 1, 1 }
+    };
 }
 
 std::vector<std::vector<int>> createM(std::vector<std::vector<int>> work_m){
-    std::vector<std::vector<int>> base_m = create_base_m(base_n, base_n);
-    //std::vector<std::vector<int>> work_m = create2DArray(newV, newV);
+    std::vector<std::vector<int>> base_m = create_base_m();
 
     int index = 0;
     int b = newV/base_n;
@@ -47,10 +54,28 @@ std::vector<std::vector<int>> createM(std::vector<std::vector<int>> work_m){
 void print(std::vector<std::vector<int>> matrix){
     for (int i = 0; i < newV; i++){
         for (int j = 0; j < newV; j++){
-            cout << matrix[i][j] << " ";
+            cout << matrix[i][j] << " ";   // Обычный вывод
+            cout << matrix[i][j] << "; ";   // Для вывода в Эксель
         }
         cout << endl;
     }
+}
+
+void print_csv(std::vector<std::vector<int>> matrix){
+
+    std::ofstream file;
+    file.open("output.csv");
+
+    file << "SCALE_STEP: " << SCALE_STEP << "; Linear (probably)" << endl;
+
+    for (int i = 0; i < newV; i++){
+        for (int j = 0; j < newV; j++){
+            file << matrix[i][j] << "; ";  
+        }
+        file << endl;
+    }
+
+    file.close();
 }
 
 int main(){
